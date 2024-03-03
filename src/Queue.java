fd3b4426808853b96bd90423c99a1b6e3a56287d
mayak123 <126418191+mayak123@users.noreply.github.com>
2024-03-03 09:34:27 +0200

    Update Queue.java

--- src/Queue.java
+++ src/Queue.java
@@ -7,6 +7,16 @@ public static void main(String[] args) {
 		q.insert(wolt2);
 		q.insert(wolt3);
 		System.out.println(find(q, 34));
+	Job job1 = new Job ("1987",30);
+		Job job2 = new Job ("9876",60);
+		Job job3 = new Job ("2765",55);
+		Job job4 = new Job ("1234", 40);
+		Queue <Job> q = new Queue ();
+		q.insert(job1);
+		q.insert(job2);
+		q.insert(job3);
+		q.insert(job4);
+		enough(q, 85);
 
 	}
 	public static String find(Queue <Wolter> q, int cc1) {
@@ -25,3 +35,25 @@ public static void main(String[] args) {
 		}
 				
 	}
+	
+	public static void enough (Queue <Job> q, int time1) {
+		q.insert(null);
+		while (q.head()!=null) {
+			if(q.head().getTime()<= time1) {
+				time1 = time1 - q.head().getTime();
+				System.out.println(time1);
+				System.out.println(q.remove().getId());
+				
+			}
+			else {
+				q.insert(q.remove());
+			}
+		}
+		q.remove();
+	}
+				
+	}
+
+
+
+
